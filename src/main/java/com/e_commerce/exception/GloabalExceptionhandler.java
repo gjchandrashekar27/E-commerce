04c1be44e5af2b4806e480b86ba7b02a0763fbad
chandrashekar Gj <chandrashekargj27@gmail.com>
2025-05-17 12:17:10 +0530

    Day 3. Admin Registration Part-1

--- src/main/java/com/e_commerce/exception/GloabalExceptionhandler.java
+++ src/main/java/com/e_commerce/exception/GloabalExceptionhandler.java
@@ -1,5 +1,14 @@
 package com.e_commerce.exception;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GloabalExceptionhandler {
+	
+	@ExceptionHandler(NoResourceFoundException.class)
+	public String handle() {
+		return "404.html";
+	}
 
 }
